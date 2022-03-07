98310d8ede74e7b8236302e57f352b2384012378
Meltwin <58667575+Meltwin@users.noreply.github.com>
2022-03-07 10:04:20 +0000

    Dé 100

--- .ccls-cache/@home@runner@TP1Q2COTEBOUVIER/lancerDes.cpp
+++ .ccls-cache/@home@runner@TP1Q2COTEBOUVIER/lancerDes.cpp
@@ -1,49 +1,26 @@
 #include <iostream>
+#include <array>
 using namespace std;
 
 int main3(void) {
   //déclaration des variables
-  int nbLancers, nb1, nb2, nb3, nb4, nb5, nb6;
-  //Initialisation des variables
-  nbLancers = 0;
-  nb1 = 0; nb2 = 0; nb3 = 0; nb4 = 0; nb5 = 0; nb6 = 0;
+  int nbLancers = 0;
+
+  array<int,100> tab = {};
 
   int valeur;
   cin >> valeur;
-  while (valeur > 0 && valeur < 7) {
+  while (valeur > 0 && valeur < 101) {
     // Traitement de la valeur lue
-    switch (valeur) {
-      case 1:
-        nb1++;
-        break;
-      case 2:
-        nb2++;
-        break;
-      case 3:
-        nb3++;
-        break;
-      case 4:
-        nb4++;
-        break;
-      case 5:
-        nb5++;
-        break;
-      case 6:
-        nb6++;
-        break;
-    }
+    tab[valeur-1]=tab[valeur-1]+1;
     nbLancers=nbLancers+1;
     cin >> valeur;
   }
   cout << endl;
   if (nbLancers != 0) {
     //Calcul des fréquences
-    cout << "f1 = " << float(nb1)/nbLancers << endl;
-    cout << "f2 = " << float(nb2)/nbLancers << endl;
-    cout << "f3 = " << float(nb3)/nbLancers << endl;
-    cout << "f4 = " << float(nb4)/nbLancers << endl;
-    cout << "f5 = " << float(nb5)/nbLancers << endl;
-    cout << "f6 = " << float(nb6)/nbLancers << endl;
+    for (int i=0; i<100; i=i+1)
+      cout <<"f" << i+1 << " = " << float(tab[i])/nbLancers << endl;
   } else {
     cout << "Aucune valeur entre 1 et 6";
   }
